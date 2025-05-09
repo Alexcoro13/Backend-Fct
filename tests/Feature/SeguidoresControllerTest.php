fcbc4a1c7ed56d1ab8babea2eea4cdaeae3c9679
Alexcorox <alex.coronetas@gmail.com>
2025-05-09 23:13:42 +0200

    Fix(Tests): Fixed Laravel Tests 2

--- tests/Feature/SeguidoresControllerTest.php
+++ tests/Feature/SeguidoresControllerTest.php
@@ -9,5 +9,8 @@ class SeguidoresControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
 }
